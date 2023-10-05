991da295540b9c0ec8d86eaf26b4db5d62749a93
winnyDiese <winnydiese@gmail.com>
2023-10-05 11:49:23 +0200

    Finish all design, wait an auther touch

--- pages/edit/[id]/index.js
+++ pages/edit/[id]/index.js
@@ -22,22 +22,31 @@ export default function Edit({params}){
             }
         })
         .catch(error=> console.log(error))
-
-
     }
 
     return (
         <>
             <Navbar />
-            <h1>Edit</h1>
-           
+            <br/>       
             <form onSubmit={editTodo}>
-                <div>
-                    <input type="text" placeholder="Titre..." value={title} onChange={e => setTitle(e.target.value)} /><br/>
-                    <input type="text" placeholder="Description..." value={todo} onChange={e => setTodo(e.target.value)} /><br/>
-                    <button type="submit">Add</button>
-                    <button type="reset">Reset</button>
+                <div className="p-10 mb-10">
+                    <div>
+                        <h1 className="uppercase -ml-6 text-2xl mb-10">Edit the task</h1>
+                    </div>
+                    <div>
+                        <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                        <input value={title} onChange={e => setTitle(e.target.value)} type="text" id="first_name" className=" w-500 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title..." required/>
+                    </div>
+                    <div>
+                        <label for="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <input value={todo} onChange={e => setTodo(e.target.value)} type="text" id="last_name" className=" w-500 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Description..." required/>
+                    </div>
+                    <div>
+                        <button type="submit" className="W-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add a task</button>
+                    </div>
+                    
                 </div>
+
             </form>
 
         </>
--- pages/index.js
+++ pages/index.js
@@ -45,27 +45,120 @@ export default function Home() {
  return (
   <>
     <Navbar />
-    <h1>All Todo</h1>
-    <br/>
+    <br/>      
+    <br/>      
+    
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg rounded-lg m-10">
+        <div>
+            <h1 className="uppercase ml-2 mt-3  text-2xl ">Edit the task</h1>
+        </div>
+        <div className="flex items-center justify-between pb-4 bg-white dark:bg-gray-900">
+            <div>
+                <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 m-5" type="button">
+                    <span className="sr-only">Action button</span>
+                    Action
+                    {/* <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg> */}
+                </button>
+                {/* <!-- Dropdown menu --> */}
+                <div id="dropdownAction" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                    <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
+                        <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reward</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Promote</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Activate account</a>
+                        </li>
+                    </ul>
+                    <div className="py-1">
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete User</a>
+                    </div>
+                </div>
+            </div>
+            <label for="table-search" className="sr-only">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    {/* <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg> */}
+                </div>
+                <input type="text" id="table-search-users" className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users" />
+            </div>
+        </div>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="p-4">
+                        <div className="flex items-center">
+                            <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <label for="checkbox-all-search" className="sr-only">checkbox</label>
+                        </div>
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Title
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Description
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                
+              {todos.map(todo => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                        <div className="flex items-center">
+                            <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <label for="checkbox-table-search-1" className="sr-only">checkbox</label>
+                        </div>
+                    </td>
+                    <td className="px-6 py-4">
+                        {todo.title}
+                    </td>
+                    <td className="px-6 py-4">
+                        {todo.todo}
+                    </td>
+                    <td className="px-6 py-4">
+                        {/* <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a> */}
 
-    <ul>
+                        
 
-      {todos.map(todo => (
-        <li key={todo._id}>{todo.title} - {todo.todo} 
-          <div> 
-            <form onSubmit={formStabilite} >
-              <input type="hidden" />
-              <button type='submit' onClick={()=>deleteTodo(todo._id)}>Remove</button>
-            </form>
-            <Link href={"/edit/id="+todo._id}>
-              <button>edit</button>
-            </Link>
-          </div>
-          <hr/>
-        </li>
-      ))}
-      
-    </ul>
+                        <form onSubmit={formStabilite} className="inline-block" >
+                          <input type="hidden" />
+                          <button 
+                            onClick={()=>deleteTodo(todo._id)} 
+                            id="dropdownActionButton" 
+                            data-dropdown-toggle="dropdownAction" 
+                            className="-ml-7 inline-flex items-center text-white hover:text-gray-500 bg-[#f45757] border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 m-5" 
+                            type="submit"
+                          >
+                            Delete
+                          </button>
+                        </form>
+                      
+                      <Link href={"/edit/id="+todo._id}>
+                        <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="-ml-3 inline-flex items-center text-white hover:text-gray-500 bg-sky-400 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 m-5" type="button">
+                          Edit
+                        </button>
+                      </Link>
+
+                    </td>
+                </tr>
+
+              ))}
+
+            </tbody>
+        </table>
+    </div>
+
+
 
   </>
  )
